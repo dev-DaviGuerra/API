e6f33dfc1e8cf0e75fe61eb36c396197e1db31c6
dev-DaviGuerra <davimguerra@gmail.com>
2025-09-27 13:10:59 -0300

    First commit

---
+++ index.py
@@ -0,0 +1,17 @@
+import requests
+from pprint import pprint
+
+
+nome = 'Keila'
+url = f'https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}'
+
+resposta = requests.get(url)
+
+try:
+    resposta.raise_for_status()
+except requests.HTTPError as e:
+    print(f'Erro no request: {e}')
+    resultado = None
+else:
+    resultado = resposta.json()
+    pprint(resultado)
