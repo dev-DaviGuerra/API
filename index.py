9bdd43d69b3df7c51037fb84f60984d54e17def1
dev-DaviGuerra <davimguerra@gmail.com>
2025-10-07 13:43:32 -0300

    Combinando requests de APIs diferentes

--- index.py
+++ index.py
@@ -1,17 +1,50 @@
 import requests
 from pprint import pprint
 
+def pegar_ids_estados():
+    url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/'
+    params = {
+    'view': 'nivelado',
+    }
+    dados_estados = fazer_request(url=url, params=params)
+    dict_estado = {}
+    for dados in dados_estados:
+        id_estado = dados['UF-id']
+        nome_estado = dados['UF-nome']
+        dict_estado[id_estado] = nome_estado
+    return dict_estado
 
-nome = 'Keila'
-url = f'https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}'
+def pegar_frequência_nome_por_estado(nome):
+    url = f'https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}'
+    params = {
+    'groupBy': 'UF',
+    }
+    dados_frequencias = fazer_request(url=url, params=params)
+    dict_frequencias = {}
+    for dados in dados_frequencias:
+        id_estado = int(dados['localidade'])
+        frequencia = dados['res'][0]['proporcao']
+        dict_frequencias[id_estado] = frequencia
+    return dict_frequencias
 
-resposta = requests.get(url)
+def fazer_request(url, params=None):
+    resposta = requests.get(url, params=params)
+    try:
+        resposta.raise_for_status()
+    except requests.HTTPError as e:
+        print(f'Erro no request: {e}')
+        resultado = None
+    else:
+        resultado = resposta.json()
+    return resultado
 
-try:
-    resposta.raise_for_status()
-except requests.HTTPError as e:
-    print(f'Erro no request: {e}')
-    resultado = None
-else:
-    resultado = resposta.json()
-    pprint(resultado)
+def main(nome):
+    dict_estados = pegar_ids_estados()
+    dict_frequencias = pegar_frequência_nome_por_estado(nome)
+    print(f'Frequência do nome {nome} nos Estados (por 100.000 habitantes)')
+    for id_estado, nome_estado in dict_estados.items():
+        frequencia_estado = dict_frequencias[id_estado]
+        print(f'->{nome_estado}: {frequencia_estado}')
+
+if __name__ == '__main__':
+    main('Davi')
